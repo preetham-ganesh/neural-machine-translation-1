35083ff1e8bcf678e654aefd4b10d7dc15e7923f
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-03-05 22:35:48 -0600

    added extract_europarl_datasets

--- codes/data_downloader_extractor.py
+++ codes/data_downloader_extractor.py
@@ -11,6 +11,7 @@ import logging
 import tensorflow_datasets as tfds
 import zipfile
 import tarfile
+import pandas as pd
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -46,13 +47,75 @@ def download_extract_datasets(european_language: str) -> None:
     file.extractall('../data/extracted_data/{}-en/europarl'.format(european_language))
     file.close()
     print('Extracted Europarl dataset for {}-en'.format(european_language))
+    print()
+
+
+def extract_europarl_datasets(european_language: str,
+                              n_sentence_pairs_per_dataset: int) -> int:
+    """Extracts the Manythings and Europarl datasets for the current european language, and splits the combined
+    sentences into multiple sub datasets for faster processing.
+
+    Args:
+        european_language: A string which contains the abbreviation for the current European language.
+        n_sentence_pairs_per_dataset: An integer which contains number of sentence pairs per dataset.
+
+    Returns:
+        An integer which contains the number of sub_datasets in the Europarl and Manythings datasets.
+    """
+    manythings_abbreviations = {'de': 'deu', 'es': 'spa', 'fr': 'fra'}
+    english_language_sentences, european_language_sentences = list(), list()
+    # Extracts manythings dataset for the current european language.
+    with zipfile.ZipFile('../data/downloaded_data/{}-en/{}-eng.zip'.format(
+            european_language, manythings_abbreviations[european_language])) as file:
+        file.extract('spa.txt', '../data/extracted_data/{}-en/manythings'.format(european_language))
+    file.close()
+    print('Extracted manythings dataset for {}-en.'.format(european_language))
+    print()
+    # Extracts Europarl dataset for the current european language.
+    file = tarfile.open('../data/downloaded_data/{}-en/{}-en.tgz'.format(european_language, european_language))
+    file.extractall('../data/extracted_data/{}-en/europarl'.format(european_language))
+    file.close()
+    print('Extracted Europarl dataset for {}-en.'.format(european_language))
+    print()
+    # Loads the manythings dataset for the current european language, as a Pandas dataframe.
+    manythings_dataset = pd.read_csv('../data/extracted_data/{}-en/manythings/{}.txt'.format(
+        european_language, manythings_abbreviations[european_language]), sep='\t', encoding='utf-8',
+        names=['en', european_language, '_'])
+    # Converts the loaded dataframe into a list of sentences.
+    english_language_sentences += list(manythings_dataset['en'])
+    european_language_sentences += list(manythings_dataset[european_language])
+    # Reads English sentences, from the Europarl dataset.
+    file = open('../data/extracted_data/{}-en/europarl/europarl-v7.{}-en.en'.format(
+        european_language, european_language))
+    english_language_sentences += file.read().split('\n')
+    file.close()
+    # Reads European language sentences, from the Europarl dataset.
+    file = open('../data/extracted_data/{}-en/europarl/europarl-v7.{}-en.{}'.format(
+        european_language, european_language, european_language))
+    european_language_sentences += file.read().split('\n')
+    file.close()
+    # Creates the following directory path if it does not exist.
+    working_directory = '../data/extracted_data/{}-en/splitted_data'.format(european_language)
+    if not os.path.isdir(working_directory):
+        os.makedirs(working_directory)
+    n_datasets = 0
+    # Iterates across the sentence pairs in the dataset, and splits the dataset.
+    for i in range(0, len(english_language_sentences), n_sentence_pairs_per_dataset):
+        current_dataset = pd.DataFrame({'en': english_language_sentences[i: i + n_sentence_pairs_per_dataset],
+                                        european_language: european_language_sentences[i: i + n_sentence_pairs_per_dataset]})
+        file_path = '{}/dataset_{}.csv'.format(working_directory, n_datasets)
+        current_dataset.to_csv(file_path, index=False)
+        print('Europarl & Manythings dataset_{} saved successfully.'.format(n_datasets))
+        print()
+        n_datasets += 1
+    return n_datasets
 
 
 def main():
     print()
     european_language = sys.argv[1]
-    download_extract_datasets(european_language)
-    print()
+    n_sentence_pairs_per_dataset = int(sys.argv[2])
+    n_datasets = extract_europarl_datasets(european_language, n_sentence_pairs_per_dataset)
 
 
 if __name__ == '__main__':
