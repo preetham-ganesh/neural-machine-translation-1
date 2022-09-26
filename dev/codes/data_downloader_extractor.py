8e4e0ca657a0456c562f23d79eae14b2fe62758a
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-09-25 23:27:28 -0500

    added download_extract_europarl_dataset function

--- dev/codes/data_downloader_extractor.py
+++ dev/codes/data_downloader_extractor.py
@@ -5,10 +5,12 @@
 
 import argparse
 
+import tarfile
 
 from utils import create_log
 from utils import log_information
-import requests
+from utils import download_tar_file
+from utils import check_directory_path_existence
 
 
 def parse_arguments() -> str:
@@ -31,11 +33,58 @@ def parse_arguments() -> str:
     return args.european_language
 
 
+def download_extract_europarl_dataset(european_language: str, language_abbreviation: str) -> None:
+    """Downloads & extracts Europarl dataset for the current european language.
+
+    Args:
+        european_language: A string which contains the name of the current european lanuage.
+        language_abbreviation: A string which contains the abbreviation for the current european language.
+    
+    Returns:
+        None.
+    """
+    # Dictionary which contains URLs for Europarl dataset language-wise.
+    dataset_urls = {
+        'german': 'https://www.statmt.org/europarl/v7/de-en.tgz', 
+        'french': 'https://www.statmt.org/europarl/v7/fr-en.tgz',
+        'italian': 'https://www.statmt.org/europarl/v7/it-en.tgz',
+        'spanish': 'https://www.statmt.org/europarl/v7/es-en.tgz'
+    }
+
+    # Downloads the tar file for current european language.
+    tar_file_path = download_tar_file(dataset_urls[european_language], 'data/original/europarl')
+
+    # Checks if the following directory path exists.
+    extracted_files_directory_path = check_directory_path_existence(
+        'data/extracted/europarl/{}'.format(language_abbreviation)
+    )
+    log_information("")
+
+    # Extracts Europarl dataset for the current european language.
+    tar_file = tarfile.open(tar_file_path)
+    tar_file.extractall(extracted_files_directory_path)
+    tar_file.close()
+
+    log_information("Extracted Europarl dataset for {} language.".format(european_language))
+    log_information("")
+
+
 def main():
     log_information('')
 
+    # Obtains the arguments.
     european_language = parse_arguments()
 
+    # Dictionary which contains abbreviations for european languages.
+    dataset_abbreviations = {
+        "german": 'de-en',
+        "italian": 'it-en',
+        "spanish": 'es-en',
+        "french": 'fr-en'
+    }
+
+    # Downloaded & Europarl dataset for the current european language.
+    download_extract_europarl_dataset(european_language, dataset_abbreviations[european_language])
 
 
 if __name__ == "__main__":
